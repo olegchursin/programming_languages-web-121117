497f02678ef159777816e49807d1e1de149118a2
Oleg Chursin <olegchursin@gmail.com>
2017-12-04 18:22:03 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -13,6 +13,13 @@ def reformat_languages(languages)
   #binding.pry
   new_hash = languages_ary
   new_hash[:ruby] = {:style => [styles_ary[0]]}
-  binding.pry
+  new_hash[:javascript] = {:style => [styles_ary]}
+  new_hash[:python] = {:style => [styles_ary[0]]}
+  new_hash[:java] = {:style => [styles_ary[0]]}
+  new_hash[:clojure] = {:style => [styles_ary[1]]}
+  new_hash[:erlang] = {:style => [styles_ary[1]]}
+  new_hash[:scala] = {:style => [styles_ary[1]]}
+  #binding.pry
+  
   new_hash
 end
